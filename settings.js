727ba09a036ddb67cd02050c367aa09239d55247
DasunMax970 <dasunmax08@gmail.com>
2025-01-01 00:23:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,6 +43,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&SZFgxKAR#6_NyKpEpowTYlvcmjcQkc4cAaDdYLM0YAn6AHB7AKBQ", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
-MONGODB: process.env.MONGODB || "mongodb+srv://manulwijethilaka42:manumd123@cluster0.le0te.mongodb.net/", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&sUYCABzT#CTyDo43SNA9VgVg4GyridEX475czDbVpcSyAelD32To", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
+MONGODB: process.env.MONGODB || "mongodb+srv://lelumamarasekaraanjanamaster:LELUM321@##@cluster0.bnndc.mongodb.net/", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
 };
